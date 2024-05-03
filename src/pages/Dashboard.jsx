7341b88751f9cf207fe7e967f53041664511e43d
Rona <longhairshortcode@gmail.com>
2024-05-02 22:19:19 -0700

    finished log out buttons, need to redirect from log in to sign up

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -44,7 +44,7 @@ useEffect(()=>{
   console.log("displayAllStudents was called")
 },[])
 
-sd
+
 //This goes here because you updateStudent function will be in table and needs to communicate/impact
 //the update component when setUpdate is triggered
   const updateStudent = (student) => {
